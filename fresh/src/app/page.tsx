a33bec70c92d268c286782fab85da97e7281916a
Jay <jay@madsq.net>
2023-07-03 01:03:28 +0900

    feat: init project

---
+++ fresh/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  let name = "park";
+  let link = "http://google.com";
+
+  return (
+    <div>
+      <h4 className="title">애플후레시</h4>
+      <p className="title-sub">by dev {name}</p>
+      <a href={link}>링크</a>
+    </div>
+  );
+}
