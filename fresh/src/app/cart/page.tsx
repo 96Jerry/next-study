7a1702a43a166f541927c4233bff422272e80045
96Jerry <junmate12@gmail.com>
2023-07-09 10:45:31 +0900

    feat: Next component

--- fresh/src/app/cart/page.tsx
+++ fresh/src/app/cart/page.tsx
@@ -1,22 +1,17 @@
+import Hello from './data'
+
+
 export default function Cart() {
     return (
       <div>
         <h4 className="title">Cart</h4>
-        <div className="cart-item">
-            <p>상품명</p>
-            <p>$40</p>
-            <p>1개</p>
-        </div>
-        <div className="cart-item">
-            <p>상품명</p>
-            <p>$40</p>
-            <p>1개</p>
-        </div>
+        <CartItem></CartItem>
+        <CartItem></CartItem>
       </div>
     )
   }
   
-  function 작명(){
+  function CartItem(){
     return(
       <div className="cart-item">
         <p>상품명</p>
